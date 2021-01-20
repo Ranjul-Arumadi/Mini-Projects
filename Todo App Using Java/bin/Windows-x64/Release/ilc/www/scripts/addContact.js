3dacf06edfebe047851a78d7153d70d28cc6b439
Ranjul Arumadi <ranjulramachandran@yahoo.com>
2021-01-21 02:06:49 +0530

    Uploaded Files

---
+++ Todo App Using Java/bin/Windows-x64/Release/ilc/www/scripts/addContact.js
@@ -0,0 +1,17 @@
+﻿
+document.getElementById("submit").addEventListener("click", add);
+
+var nameArray = [];
+var numberArray = [];
+
+function add() {
+    console.log("in add");
+
+    var name = document.getElementById('box1').value;
+    nameArray.push(name);
+    console.log(nameArray);
+
+    var number = document.getElementById('box2').value;
+    numberArray.push(number);
+    console.log(numberArray);
+}

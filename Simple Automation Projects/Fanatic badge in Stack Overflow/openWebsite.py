cd86e71d057a4b9befd401e619fd386ec2e24362
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-07-21 13:00:54 +0530

    Update and rename Simple Automation Projects/A/a.txt/B/b.txt to Simple Automation Projects/Fanatic badge in Stack Overflow/openWebsite.py

---
+++ Simple Automation Projects/Fanatic badge in Stack Overflow/openWebsite.py
@@ -0,0 +1,10 @@
+import webbrowser
+webbrowser.open("https://stackoverflow.com/INSERT YOUR URL HERE", new=1)
+
+'''
+Notes:
+
+Call webbrowser.open(url, new=1) to open the url in the default web browser of the system. 
+new=1 opens a new browser window, new=0 opens in the same browser window, 
+and new=2 opens in a new tab.
+'''
